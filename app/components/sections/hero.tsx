6a063316b7b623cfff2620194ac83f4259c2e511
LucianoMazar <lmazaraojr@gmail.com>
2026-02-02 16:20:49 -0300

    style: adjust hero gradient colors

--- app/components/sections/hero.tsx
+++ app/components/sections/hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero(){
     return(
-        <section className="flex p-12 mt-24 bg-linear-to-l from-hero-gradient-primary via-hero-gradient-secundary to-hero-gradient-secundary">
+        <section className="flex p-12 mt-24 bg-linear-to-l from-hero-gradient-primary via-hero-gradient-secundary to-hero-gradient-primary">
             <div>
                 
             </div>
